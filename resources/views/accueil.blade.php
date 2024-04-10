ef76d545822e3a15d4a4ad00f4f4e6217f74bc57
zoeleca <zoennts@gmail.com>
2024-04-10 14:48:11 +0000

    new view created

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -7,7 +7,9 @@
     <h1>Posts</h1>
     @foreach ($posts as $post)
         <div>
-            <h2>{{ $post->author_id }}</h2>
+            <h2>{{ $post->author->name }}</h2>
+            
+            <img src='{{ $post->url_img }}'/>
             <p>{{ $post->post_content }}</p>
         </div>
     @endforeach
