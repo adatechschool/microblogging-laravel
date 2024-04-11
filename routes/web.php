d018b866880d5b53f9f375526b6817020dfa2c1f
MorganeLM <lets.morganise.it@gmail.com>
2024-04-11 09:44:49 +0000

    Co-authored-by: Zoe L. <zoeleca@users.noreply.github.com> Co-authored-by: psykokwak0912 <psykokwak0912@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -19,4 +18,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/posts', [PostController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::get('/home', function() {
+    return view('accueil'); // Return the home.blade.php view
+});
