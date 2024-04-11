8b80b12ab68a31095e03ff07877d949f2316e207
MorganeLM <lets.morganise.it@gmail.com>
2024-04-11 13:34:04 +0000

    rename profile -> account

--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/feed', function () {
+    return view('feed');
+})->middleware(['auth', 'verified'])->name('feed');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/account', [ProfileController::class, 'edit'])->name('account.edit');
+    Route::patch('/account', [ProfileController::class, 'update'])->name('account.update');
+    Route::delete('/account', [ProfileController::class, 'destroy'])->name('account.destroy');
 });
 
 require __DIR__.'/auth.php';
