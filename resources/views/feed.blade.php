6976cf08dbc602005ee8cdff7fb0cedc07177b4d
zoeleca <zoennts@gmail.com>
2024-04-11 14:08:39 +0000

    css updated !

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
 
     <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-7">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     {{ __("You're logged in!") }}
@@ -15,24 +15,26 @@
         </div>
     </div>
     <div class="py-12">
-
-        <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 text-gray-900 dark:text-gray-100">
-                <div class ='flex flex-col items-center justify-center border'>
-
-                    @foreach ($posts as $post)
-                        <div class='flex'>
-                            <h1 class="text-3xl font-bold underline">
-                                Hello world!
-                            </h1>
-                            <h1>{{ $post->author->name }}</h1>
-
-                            <img src='{{ $post->url_img }}' />
-                            <p>{{ $post->post_content }}</p>
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800  shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6">
+                        @foreach ($posts as $post)
+                        <div class="border border-gray-300 dark:border-gray-700 rounded-lg overflow-hidden">
+                            <div class="p-4">
+                            <h1 class="text-lg font-semibold">{{ $post->author->name }}</h1>
+                            <h1 class="text-sm font">{{ $post->created_at->format('F j, Y')}}</h1>
                         </div>
-                    @endforeach
-                </div>
+                            <div class="relative overflow-hidden">
+                                <img src="{{ $post->url_img }}" alt="Post Image" class="w-full h-64 object-cover">
+                            </div>
+                            <div class="p-4">
+                                <p class="text-gray-700 dark:text-gray-300">{{ $post->post_content }}</p>
+                            </div>
+                        </div>
+                        @endforeach
+                    </div>
             </div>
         </div>
-    </div>
+</div>
 </x-app-layout>
