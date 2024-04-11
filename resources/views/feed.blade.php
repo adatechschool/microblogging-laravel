269a3eb25d389be4ffd115ffa4450d97c7b063ea
zoeleca <zoennts@gmail.com>
2024-04-11 14:28:44 +0000

    'addpost' form created and added to feed

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -14,6 +14,10 @@
             </div>
         </div>
     </div>
+
+  <!-- Include the form for creating a new post -->
+  @include('post.addPost')
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800  shadow-sm sm:rounded-lg">
