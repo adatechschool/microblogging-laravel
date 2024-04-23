22c7b75b30efc39d5cdeb3faf7da704269f75699
zoeleca <zoennts@gmail.com>
2024-04-23 10:36:00 +0000

    updated the profile view 'show'

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,12 +1,12 @@
 {{-- @extends('layouts.app') --}}
 {{-- @section('slot') --}}
 <x-app-layout>
-    <img src={{$profile->background_picture}} alt="">
+    <img src={{$profile->background_picture}} alt="my Background Picture">
     <div class="container mx-auto px-4 py-6 bg-white dark:bg-gray-800">
         <img src={{$profile->profile_picture}} alt="my Profile Picture">
         <h1 class="text-3xl font-bold text-gray-800 dark:text-white mb-2">Profil de {{ $user->name }}</h1>
         <br>
-        <p class="text-xl text-gray-600 dark:text-gray-300">Email : {{ $profile->email }}</p>
+        <p class="text-xl text-gray-600 dark:text-gray-300">Email : {{ $user->email }}</p>
         <p class="text-xl text-gray-600 dark:text-gray-300">Biography : {{ $profile->biography }}</p>
         <p class="text-xl text-gray-600 dark:text-gray-300">Github : {{ $profile->github_link }}</p>
         <!-- Ajouter plus d'éléments de profil si nécessaire -->
