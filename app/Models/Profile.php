c9af9a37a8195c78a00ed521fd372f205cac62e9
zoeleca <zoennts@gmail.com>
2024-04-23 10:34:36 +0000

    added info to profile Model (what the table post contains (columns)

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -13,4 +13,21 @@ class Profile extends Model
     {
         return $this->belongsTo(User::class, 'id');
     }
+
+    /**
+     * Mass assignement : when you send an array to the model creation
+     * Setting differents fields in a single go. 
+     * 
+     * $fillable : allowing updating
+     * @var array //data type variable + specify $fillable is an array
+     */
+
+    protected $fillable = [
+      'user_id', // When creating or updating profile, assign the value to user_id
+      'profile_picture',
+      'background_picture',
+      'biography',
+      'github_link',
+  ];
+  
 }
