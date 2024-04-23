9adfcba3b550323b5026d218a7640bb1f892b96a
zoeleca <zoennts@gmail.com>
2024-04-23 09:31:35 +0000

    added user_id column in factory

--- database/factories/ProfileFactory.php
+++ database/factories/ProfileFactory.php
@@ -14,6 +14,9 @@ class ProfileFactory extends Factory
     public function definition(): array
     {
         return [
+            'user_id' => function () {
+                return \App\Models\User::factory()->create()->id;
+            }, //not sure if the Profilefactory is useful now 
             'created_at' => fake() -> dateTime($max = 'now', $timezone = null),
             'updated_at' => fake() -> dateTime($max = 'now', $timezone = null),
             'profile_picture' => fake() -> imageUrl ($width = 128, $height = 128),
