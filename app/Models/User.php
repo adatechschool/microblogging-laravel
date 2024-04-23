2ca0e3750385503bf283b658422473c22c19bbb1
zoeleca <zoennts@gmail.com>
2024-04-23 09:29:56 +0000

    added a function profile (one to one relationship with profile model)

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,11 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    public function profile()
+    {
+        return $this->hasOne(Profile::class);
+    }
+
     use HasApiTokens, HasFactory, Notifiable;
 
     /**
