310d7c23ca518b8881153299020a560410b26b54
psykokwak0912 <gdepecker1203@gmail.com>
2024-04-24 14:07:18 +0000

    ajoute des tag a la page feed

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -20,22 +20,27 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800  shadow-sm sm:rounded-lg">
+            <div class="bg-white dark:bg-gray-800 shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-6">
                         @foreach ($posts as $post)
                             <div class="border border-gray-300 dark:border-gray-700 rounded-lg overflow-hidden">
                                 <div class="p-4">
-                                    <!-- Wrap the author's name in an anchor tag with a link to the user profile route -->
-                                    <a href="{{ route('user.profile', ['user_id' => $post->author->id]) }}"
-                                        class="text-lg font-semibold">
+                                    <a href="{{ route('user.profile', ['user_id' => $post->author->id]) }}" class="text-lg font-semibold">
                                         {{ $post->author->name }}
                                     </a>
-
                                     <h1 class="text-sm font">{{ $post->created_at->format('F j, Y') }}</h1>
-                                </div>
-                                <div class="relative overflow-hidden">
-                                    <img src="{{ $post->url_img }}" alt="Post Image" class="w-full h-64 object-cover">
+                                    
+                                    <div class="relative overflow-hidden">
+                                        <img src="{{ $post->url_img }}" alt="Post Image" class="w-full h-64 object-cover">
+                                    </div>
+                                    <div class="mt-2">
+                                        @if ($post->tags)
+                                            @foreach ($post->tags as $tag)
+                                                <span class="inline-block bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-gray-200 px-2 py-1 rounded-full text-xs">{{ $tag->tag_name }}</span>
+                                            @endforeach
+                                        @endif
+                                    </div>
                                 </div>
                                 <div class="p-4">
                                     <p class="text-gray-700 dark:text-gray-300">{{ $post->post_content }}</p>
@@ -46,4 +51,5 @@
                 </div>
             </div>
         </div>
+    </div>
 </x-app-layout>
