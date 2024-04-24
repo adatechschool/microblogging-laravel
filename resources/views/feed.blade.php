14164917a884cf23569db68b36c2663df6023971
julieATS <julie-bonnin97@hotmail.fr>
2024-04-24 14:45:42 +0200

    added condition to not display default alt if their is no image url in the database

--- resources/views/feed.blade.php
+++ resources/views/feed.blade.php
@@ -29,9 +29,11 @@
                             <h1 class="text-lg font-semibold">{{ $post->author->name }}</h1>
                             <h1 class="text-sm font">{{ $post->created_at->format('F j, Y')}}</h1>
                         </div>
+                        @if (!empty($post->url_img))
                             <div class="relative overflow-hidden">
                                 <img src="{{ $post->url_img }}" alt="Post Image" class="w-full h-64 object-cover">
                             </div>
+                            @endif 
                             <div class="p-4">
                                 <p class="text-gray-700 dark:text-gray-300">{{ $post->post_content }}</p>
                             </div>
