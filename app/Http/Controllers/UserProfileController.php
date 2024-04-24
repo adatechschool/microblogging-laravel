0c49af8c3d36912f3180ffbbeff5dfd46328f46a
zoeleca <zoennts@gmail.com>
2024-04-24 09:28:33 +0000

    function 'showProfile' that display the informations and posts of a user

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -4,18 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Post;
 
 class UserProfileController extends Controller
 {
+    //  Function to show the profile of the logged user (and edit it)
     public function show()
     {
         $user = auth()->user();
-        $profile = $user->profile; 
+        $profile = $user->profile;
 
         return view('show', [
             'user' => $user,
             'profile' => $profile,
         ]);
     }
+
+    //  Function to show the profile of any user
+    public function showProfile($userId)
+{
+    $user = User::findOrFail($userId);
+    $profile = $user->profile;
+    $posts = $user->posts; // Fetch user's posts
+
+    return view('profile', compact('user', 'profile', 'posts'));
 }
 
+}
